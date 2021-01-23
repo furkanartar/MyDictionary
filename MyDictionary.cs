23fadf2c82c4a6d9eb716a2a150d5bc2a6dd69fb
Furkan ARTAR <asosyalgenctvtr@gmail.com>
2021-01-23 20:33:34 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿namespace MyDictionary
+{
+    public class MyDictionary<T, X>
+    {
+        private T[] _firstArray;
+        private T[] _secondArray;
+
+        private T[] _tempFirtsArray;
+        private T[] _tempSecondArray;
+
+        public MyDictionary()
+        {
+            _firstArray = new T[0];
+            _secondArray = new T[0];
+        }
+
+        public void Add(T firstItem, X secondItem)
+        {
+            _tempFirtsArray = _firstArray;
+            _tempSecondArray = _secondArray;
+
+            _firstArray = new T[_tempFirtsArray.Length + 1];
+            _secondArray = new T[_tempSecondArray.Length + 1];
+
+            for (int i = 0; i < _tempFirtsArray.Length; i++)
+            {
+                _firstArray[i] = _tempFirtsArray[i];
+            }
+
+            for (int i = 0; i < _tempSecondArray.Length; i++)
+            {
+                _secondArray[i] = _tempSecondArray[i];
+            }
+
+            _firstArray[_firstArray.Length - 1] = firstItem;
+            _secondArray[_secondArray.Length - 1] = firstItem;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> days = new Dictionary<int, string>();
+            days.Add(0, "Pazartesi");
+
+            MyDictionary<int, string> days2 = new MyDictionary<int, string>();
+            days2.Add(1, "Pazartesi");
+            days2.Add(2, "Salı");
+            days2.Add(3, "Çarşamba");
+            days2.Add(4, "Perşembe");
+            days2.Add(5, "Cuma");
+            days2.Add(6, "Cumartesi");
+            days2.Add(7, "Pazar");
+        }
+    }
+}
